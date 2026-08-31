c5b0be30bd3729700b1e06cede885706ab5fd212
Dasindu Rajapaksha <dasindudrajapaksha@gmail.com>
2026-08-31 17:15:53 +0530

    Configure email-service

--- src/services/emailservice.js
+++ src/services/emailservice.js
@@ -5,7 +5,7 @@ const sendVerificationEmail = async (email, token) => {
     `http://localhost:5000/api/auth/verify-email/${token}`;
 
   await transporter.sendMail({
-    from: `"SyncBoard" <${process.env.EMAIL_USER}>`,
+    from: `"SyncBoard" <${process.env.EMAIL_USER || "no-reply@syncboard.local"}>`,
     to: email,
     subject: "Verify your SyncBoard email",
     text: `Verify your email by visiting: ${verificationUrl}`,
